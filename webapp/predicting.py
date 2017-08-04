aa16aae7bf87931ce55dc90b8d57c2ef1047aea6
Alejandro Solano <asolanopf@gmail.com>
2017-08-04 14:41:51 +0200

    fix some bugs

--- webapp/predicting.py
+++ webapp/predicting.py
@@ -1,9 +1,8 @@
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' # ignore debugging logs
+from os import environ
+environ['TF_CPP_MIN_LOG_LEVEL'] = '3' # ignore debugging logs
 
 import tensorflow as tf
 import numpy as np
-import cv2
 
 from config import *
 
@@ -15,7 +14,6 @@ saver = tf.train.import_meta_graph('./models/inception_resnet_v2/model.ckpt.meta
 input_image_string = tf.get_default_graph().get_tensor_by_name('input_image_string:0')
 probs = tf.get_default_graph().get_tensor_by_name('probs:0')
 
-
 ### CLASSES ###
 with open('./models/imagenet-classes.txt') as f:
     classes = f.read().splitlines()
@@ -23,7 +21,6 @@ with open('./models/imagenet-classes.txt') as f:
 
 
 def evaluate(filename):
-
     ### LOAD IMAGE ###
     with open(UPLOAD_FOLDER + '/' + filename, 'rb') as f:
         image_string = f.read()
@@ -40,4 +37,4 @@ def evaluate(filename):
         pred_class = classes[pred_idx - 1] # from 1001 to 1000 classes
         pred_score = np.around(100*prob_values[0][pred_idx], decimals=2) # two decimals
 
-        return list(pred_class), list(pred_score
+        return list(pred_class), list(pred_score)
--- webapp/webapp.py
+++ webapp/webapp.py
@@ -22,13 +22,13 @@ def index():
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
-            flash('No file part')
+            print('No file part')
             return redirect(request.url)
         file = request.files['file']
         # if user does not select file, browser also
         # submit a empty part without filename
         if file.filename == '':
-            flash('No selected file')
+            print('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
