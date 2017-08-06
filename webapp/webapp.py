c38a5847a9fe8a1a00aac6a2c4dc859f1302e8a8
Alejandro Solano <asolanopf@gmail.com>
2017-08-06 18:20:02 +0200

    change predicting script to predictor class

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -11,7 +11,8 @@ if not os.path.exists(UPLOAD_FOLDER):
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 import time
-import predicting
+from predicting import Predictor
+predictor = Predictor()
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -40,7 +41,10 @@ def index():
 @app.route('/uploaded')
 def uploaded():
     begin = time.time()
-    pred_class, pred_score = predicting.evaluate(filename=request.args.get('filename'))
+    pred_class, pred_score = predictor.evaluate(
+        filename=request.args.get('filename'),
+        model_name='inception_resnet_v2',
+        graph_type='checkpoints')
     end = time.time()
     print("\nElapsed time: %0.5f seconds." % (end-begin))
     
